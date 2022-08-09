1a6dda1f32bc50a7aba05666c7ffb6b027d0fe7e
Amr Gaber <amrtgaber@protonmail.com>
2022-08-08 21:37:52 -0700

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,9 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app: INestApplication = await NestFactory.create(AppModule);
+  const app: INestApplication = await NestFactory.create(AppModule, {
+    cors: true,
+  });
 
   configureSwagger(app);
 
